13ddf37b65e6135b7d9daf29a3c134ccf75f186b
Maria Rabozzi <maria.rabozzi@globant.com>
2025-07-24 12:07:48 -0300

    fix string

--- CountryExplorer/Localization/Localizated.swift
+++ CountryExplorer/Localization/Localizated.swift
@@ -24,7 +24,7 @@ extension String {
     static var leftDrive: String { "🚨left".localized }
     static var rightDrive: String { "🚨right".localized }
     static var coatOfArmsTitle: String { "🚨coat_of_arms".localized }
-    static var selectLanguajeTitle: String { "🚨selectLanguaje".localized }
+    static var selectLanguajeTitle: String { "🚨SelectLanguage".localized }
 }
 
 extension String {
